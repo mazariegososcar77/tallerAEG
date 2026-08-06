b30709cb78abe5cab32bac7070221f64cf182424
Misael Chavez <mchavez.munoz@outlook.com>
2026-08-05 21:20:25 -0600

    chore: remover módulo de invoices paralelo (developer ya lo tiene vía Oscar)

--- backend/migraciones/017_invoice_items_type.sql
+++
@@ -1,8 +0,0 @@
--- =====================================================================
--- 017_invoice_items_type.sql  -  Tipo de linea en facturas
--- Digifact exige por item si es "Bien" o "Servicio" (afecta UnitOfMeasure
--- y el tratamiento fiscal). Ejecutar despues de 016_invoices_seed.sql
--- =====================================================================
-
-ALTER TABLE invoice_items
-  ADD COLUMN item_type ENUM('bien','servicio') NOT NULL DEFAULT 'servicio' AFTER description;
